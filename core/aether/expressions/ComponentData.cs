486f560f9c059e86a18af26938075040934b8ac3
theryo12 <theryo12@gmail.com>
2024-12-01 03:24:07 +0300

    update ComponentData.cs

--- core/aether/expressions/ComponentData.cs
+++ core/aether/expressions/ComponentData.cs
@@ -1,4 +1,5 @@
 using System;
+using Terraria.DataStructures;
 
 namespace Aether.Expressions;
 
@@ -12,11 +13,20 @@ public struct ComponentData<T>(uint size) where T : unmanaged
   /// <summary>
   /// Sparse array mapping of entity ID to the location of data.
   /// </summary>
-  private readonly uint[] _sparse = new uint[size];
+  private uint[] _sparse = new uint[size];
 
-  public void AllocateData(uint size)
+  /// <summary>
+  /// Tracks the next avaliable index for new data.
+  /// </summary>
+  private uint _nextAvailableIndex = 0;
+
+  private void EnsureCapacity(uint requiredSize)
   {
-    _data = new T[size];
+    if (requiredSize > _data.Length)
+      ResizeData(requiredSize * 2);
+
+    if (requiredSize > _sparse.Length)
+      ResizeSparse(requiredSize * 2);
   }
 
   public void ResizeData(uint newSize)
@@ -24,25 +34,25 @@ public struct ComponentData<T>(uint size) where T : unmanaged
     Array.Resize(ref _data, (int)newSize);
   }
 
-  public readonly void SetEntityDataLocation(uint entityId, uint location)
+  public void ResizeSparse(uint newSize)
   {
-    if (entityId >= _sparse.Length)
-      throw new ArgumentOutOfRangeException(nameof(entityId));
-
-    _sparse[entityId] = location;
+    Array.Resize(ref _sparse, (int)newSize);
   }
 
-  public readonly T this[uint entityId, uint offset]
+  public readonly ref T Get(uint entityId)
   {
-    get
-    {
-      uint location = _sparse[entityId];
-      return _data[location + offset];
-    }
-    set
-    {
-      uint location = _sparse[entityId];
-      _data[location + offset] = value;
-    }
+    if (entityId >= _sparse.Length || _sparse[entityId] == 0)
+      throw new ArgumentOutOfRangeException(nameof(entityId), "Entity does not have assigned data.");
+    return ref _data[_sparse[entityId]];
+  }
+
+  public void Set(uint entityId, T value)
+  {
+    EnsureCapacity(entityId + 1);
+
+    if (_sparse[entityId] == 0 && _nextAvailableIndex < _data.Length)
+      _sparse[entityId] = _nextAvailableIndex++;
+
+    _data[_sparse[entityId]] = value;
   }
 }
